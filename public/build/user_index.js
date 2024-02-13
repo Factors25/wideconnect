5bb963719979ea082de7d9840ecaf72b6926f9d3
FactorSpeed <alexis25.py@gmail.com>
2024-02-13 22:46:41 +0100

    [master] Initial commit

---
+++ public/build/user_index.js
@@ -0,0 +1,29 @@
+"use strict";
+(self["webpackChunkcraft"] = self["webpackChunkcraft"] || []).push([["user_index"],{
+
+/***/ "./assets/js/components/user/user_index.js":
+/*!*************************************************!*\
+  !*** ./assets/js/components/user/user_index.js ***!
+  \*************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _user_util__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./user_util */ "./assets/js/components/user/user_util.js");
+
+document.addEventListener('DOMContentLoaded', function () {
+  (0,_user_util__WEBPACK_IMPORTED_MODULE_0__.datatable)();
+  (0,_user_util__WEBPACK_IMPORTED_MODULE_0__.createModal)({
+    datatable: true
+  });
+});
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["vendors-node_modules_jquery_dist_jquery_js","vendors-node_modules_core-js_modules_es_array_concat_js-node_modules_core-js_modules_es_array-9caedb","assets_js_components_user_user_util_js"], () => (__webpack_exec__("./assets/js/components/user/user_index.js")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidXNlcl9pbmRleC5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7OztBQUFtRDtBQUVuREUsUUFBUSxDQUFDQyxnQkFBZ0IsQ0FBQyxrQkFBa0IsRUFBRSxZQUFNO0VBQ2hERixxREFBUyxDQUFDLENBQUM7RUFDWEQsdURBQVcsQ0FBQztJQUFDQyxTQUFTLEVBQUU7RUFBSSxDQUFDLENBQUM7QUFDbEMsQ0FBQyxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vY3JhZnQvLi9hc3NldHMvanMvY29tcG9uZW50cy91c2VyL3VzZXJfaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHtjcmVhdGVNb2RhbCwgZGF0YXRhYmxlfSBmcm9tIFwiLi91c2VyX3V0aWxcIjtcclxuXHJcbmRvY3VtZW50LmFkZEV2ZW50TGlzdGVuZXIoJ0RPTUNvbnRlbnRMb2FkZWQnLCAoKSA9PiB7XHJcbiAgICBkYXRhdGFibGUoKTtcclxuICAgIGNyZWF0ZU1vZGFsKHtkYXRhdGFibGU6IHRydWV9KTtcclxufSkiXSwibmFtZXMiOlsiY3JlYXRlTW9kYWwiLCJkYXRhdGFibGUiLCJkb2N1bWVudCIsImFkZEV2ZW50TGlzdGVuZXIiXSwic291cmNlUm9vdCI6IiJ9
