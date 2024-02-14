0e8174ecf635a5da262a1c302ec559cc654ee834
FactorSpeed <alexis25.py@gmail.com>
2024-02-14 11:29:38 +0100

    [master] Initial commit

--- src/Controller/Home/HomeController.php
+++ src/Controller/Home/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Controller\Home;
 use App\Service\Generic\CrudGeneric;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends CrudGeneric
 {
---
+++ src/Controller/Security/PrivacyPolicyController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Security;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PrivacyPolicyController extends AbstractController
+{
+    /**
+     * @return Response
+     */
+    #[Route(path: '/privacy-policy', name: 'privacy_policy')]
+    public function index(): Response
+    {
+        return $this->render('security/privacy-policy/privacy_policy.html.twig');
+    }
+}
