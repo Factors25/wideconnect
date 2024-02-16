cd3e1f482b230beca33931d3a5023803bf416a4b
FactorSpeed <alexis25.py@gmail.com>
2024-02-16 09:44:09 +0100

    [master] Attribute

--- src/Controller/Security/RegisterController.php
+++ src/Controller/Security/RegisterController.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 class RegisterController extends AbstractController
